1b88811758a5176381decd9263fd952bc1c6a704
rocha.porto <rocha.porto@808423f4-a80b-11dd-9c66-c9f8973bfffa>
2008-11-07 18:00:40 +0000

    added prototype similar to google apps dashb
    git-svn-id: http://opensoaring.googlecode.com/svn/trunk@10 808423f4-a80b-11dd-9c66-c9f8973bfffa

--- gwt/src/opensoaring/client/OpenSoaring.java
+++ gwt/src/opensoaring/client/OpenSoaring.java
@@ -1,64 +1,53 @@
 package opensoaring.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.DeckPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class OpenSoaring implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    Image img = new Image("opensoaringlogo.png");
-    Button button = new Button("Click me");
-    
-    // We can add style names
-    button.addStyleName("pc-template-btn");
-    // or we can set an id on a specific element for styling
-    img.getElement().setId("pc-template-img");
-    
-    VerticalPanel vPanel = new VerticalPanel();
-    vPanel.setWidth("100%");
-    vPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-    vPanel.add(img);
-    vPanel.add(button);
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private HorizontalPanel navPanel = new HorizontalPanel();
+	private HorizontalPanel headerPanel = new HorizontalPanel();
+	private DeckPanel contentPanel = new DeckPanel();
+	
+	private ToggleButton mainNavButton = new ToggleButton("Home");
+	private ToggleButton schedNavButton = new ToggleButton("Schedule");
+	private ToggleButton flightNavButton = new ToggleButton("Flight Info");
+	
+	public void onModuleLoad() {
 
-    // Add image and button to the RootPanel
-    RootPanel.get().add(vPanel);
-
-    // Create the dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Welcome to GWT mine!");
-    dialogBox.setAnimationEnabled(true);
-    Button closeButton = new Button("close");
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.setWidth("100%");
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-    dialogVPanel.add(closeButton);
-
-    closeButton.addClickListener(new ClickListener() {
-      public void onClick(Widget sender) {
-        dialogBox.hide();
-      }
-    });
-
-    // Set the contents of the Widget
-    dialogBox.setWidget(dialogVPanel);
-    
-    button.addClickListener(new ClickListener() {
-      public void onClick(Widget sender) {
-        dialogBox.center();
-        dialogBox.show();
-      }
-    });
-  }
+		// Header Panel
+		headerPanel.setStyleName("headerPanel");
+		
+		// Navigation Panel
+		HorizontalPanel navItemsPanel = new HorizontalPanel();
+		mainNavButton.setStylePrimaryName("navButton");
+		schedNavButton.setStylePrimaryName("navButton");
+		flightNavButton.setStylePrimaryName("navButton");
+		navItemsPanel.add(mainNavButton);
+		navItemsPanel.add(schedNavButton);
+		navItemsPanel.add(flightNavButton);
+		navPanel.add(navItemsPanel);
+		navPanel.setStyleName("navPanel");
+		
+		// Content Panel + Adding Applications
+		contentPanel.add(new ScheduleApp("Home"));
+		contentPanel.add(new ScheduleApp("Schedule"));
+		contentPanel.add(new ScheduleApp("Flight Info"));
+		contentPanel.showWidget(0);
+		contentPanel.setStyleName("contentPanel");
+		
+		// Main Panel
+		mainPanel.add(headerPanel);
+		mainPanel.add(navPanel);
+		mainPanel.add(contentPanel);
+		mainPanel.setStyleName("mainPanel");
+		
+		RootPanel.get().add(mainPanel);		
+	  
+	}
 }
---
+++ gwt/src/opensoaring/client/ScheduleApp.java
@@ -0,0 +1,61 @@
+package opensoaring.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DeckPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.ToggleButton;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class ScheduleApp extends Composite {
+	
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private HorizontalPanel headerPanel = new HorizontalPanel();
+	private VerticalPanel timedSchedPanel = new VerticalPanel();
+	private HorizontalPanel timedSchedNavPanel = new HorizontalPanel();
+	private DeckPanel timedSchedContentPanel = new DeckPanel();
+	
+	private ToggleButton fourDaysNavButton = new ToggleButton("4 Days");
+	private ToggleButton weekNavButton = new ToggleButton("Week");
+	private ToggleButton monthNavButton = new ToggleButton("Month");
+	
+	public ScheduleApp(String caption) {
+	
+		// App Header Panel
+		HTML appHeaderTitle = new HTML(caption);
+		appHeaderTitle.setStyleName("appHeaderTitle");
+		headerPanel.add(appHeaderTitle);
+		headerPanel.setStyleName("appHeaderPanel");
+		mainPanel.add(headerPanel);
+		
+		// App Navigation Panel
+		HorizontalPanel navItemsPanel = new HorizontalPanel();
+		fourDaysNavButton.setStylePrimaryName("navSchedButton");
+		weekNavButton.setStylePrimaryName("navSchedButton");
+		monthNavButton.setStylePrimaryName("navSchedButton");
+		navItemsPanel.add(fourDaysNavButton);
+		navItemsPanel.add(weekNavButton);
+		navItemsPanel.add(monthNavButton);
+		timedSchedNavPanel.add(navItemsPanel);
+		timedSchedNavPanel.setStyleName("navSchedPanel");		
+		timedSchedPanel.add(timedSchedNavPanel);		
+		
+		// App Content Panel(s)
+		timedSchedContentPanel.add(new HTML("Text"));
+		timedSchedContentPanel.showWidget(0);
+		fourDaysNavButton.setDown(true);
+		timedSchedContentPanel.setStyleName("contentSchedPanel");
+		timedSchedPanel.add(timedSchedContentPanel);
+		
+		// App Panel
+		timedSchedPanel.setStyleName("schedPanel");
+		mainPanel.add(timedSchedPanel);
+		
+		// Finish up the composite
+		initWidget(mainPanel);
+		
+		setStyleName("appPanel");
+	}
+	
+}
