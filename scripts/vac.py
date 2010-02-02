8fa400417b828360db0d6df0d28904f026c5e9bd
Ricardo Rocha <rocha.porto@gmail.com>
2010-02-02 11:09:07 +0100

    added verbose, quiet, merge options and functionality

--- scripts/vac.py
+++ scripts/vac.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
+import commands
 import os
-from urllib.request import FancyURLopener
+import urllib2
+import shutil
 import sys
 from optparse import OptionParser
 from string import ascii_uppercase
 
+VAC_ALL_FILE = "vac_all.pdf"
 BASE_URL = "https://www.sia.aviation-civile.gouv.fr/aip/enligne/PDF_AIPparSSection/VAC/AD/2/1002_AD-2.%s.pdf"
+DW_ICAO_LIST = ["LFAB", "LFAC"]
 
 usage = "usage: %prog [options] directory"
 description = """
@@ -13,9 +17,9 @@ Downloads all french VAC charts into the given directory.
 <directory> must be an existing directory.
 """
 
-http://docs.python.org/library/urllib2.html
-
 parser = OptionParser(usage=usage, description=description, version="0.1a")
+parser.add_option("-a", "--all", action="store_true", dest="all")
+parser.add_option("-m", "--merge", action="store_true", dest="merge")
 parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
 parser.add_option("-q", "--quiet", action="store_true", dest="quiet")
 
@@ -26,31 +30,64 @@ def main():
         sys.exit(2)
     
     directory = args[0]
-    if not os.path.isdir(directory):
-        print "ERROR: Please provide an existing directory"
-        parser.print_help()
-        sys.exit(2)
+    if os.path.isdir(directory):
+        overwrite_ok = raw_input("Will delete/recreate directory '%s'... continue? [Y/N] " % directory).upper()
+        while overwrite_ok not in ["Y", "N"]:
+            overwrite_ok = raw_input("Will delete/recreate directory '%s'... continue? [Y/N] " % directory).upper()
+        if not overwrite_ok == "Y":
+            fail("Canceled by user")
+    shutil.rmtree(directory)
+    os.makedirs(directory)
 
-    if not options.quiet:
-        print "Fetching VAC charts"
+    icao_code_list = DW_ICAO_LIST
+    info("Fetching VAC charts", options)
+    if options.all:
+        icao_code_list = []
+        for c1 in ascii_uppercase:
+            for c2 in ascii_uppercase:
+                icao_code_list.append("LF%s%s" % (c1, c2))
+    for icao_code in icao_code_list:
+        vac_url = BASE_URL % icao_code
+        try:
+            resp = urllib2.urlopen(vac_url)
+            vac_file = "%s/%s.pdf" % (directory, icao_code)
+            vac_fileh = open(vac_file, "w")
+            vac_fileh.write(resp.read())
+            resp.close()
+            vac_fileh.close()
+        except urllib2.HTTPError, exc:
+            if exc.code == 404:
+                continue
+            fail("Could not retrieve data: %s" % vac_url)
+        except urllib2.URLError, exc:
+            fail("Could not retrieve data: %s" % vac_url)
+        except IOError, exc:
+            fail("Failed to write to file: %s" % vac_file)
 
-    url_opener = FancyURLopener()
-    for c1 in ascii_uppercase:
-        for c2 in ascii_uppercase:
-            icao_code = "LF%s%s" % (c1, c2)
-            vac_url = BASE_URL % icao_code
-            url_opener.retrieve(vac_url, filename="%s/%s.pdf" % (directory, icao_code))
-                
-            if options.verbose:
-                print "Fetched VAC chart for %s" % icao_code
-                print "( %s )" % vac_url
-                print res
-            elif not options.quiet:
-                sys.stdout.write(".")
-                sys.stdout.flush()
-                
-    if not options.quiet:
-        print "\nDone."
+        if options.verbose:
+            info("Fetched VAC chart for %s" % icao_code, options, True)
+            info("( %s )" % vac_url, options, True)
+        elif not options.quiet:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+    info("Done.", options)
+
+    if options.merge:
+        info("Merging all VAC charts into one pdf file...", options)
+        cmd = "gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=%s %s" \
+              % ("%s/%s" % (directory, VAC_ALL_FILE), " ".join(map(lambda c: "%s/%s.pdf" % (directory, c), icao_code_list)))
+        info(cmd, options, True)
+        (code, output) = commands.getstatusoutput(cmd)
+        if code != 0:
+            fail("Failed to generate all VAC charts pdf file...\n%s" % output)
+        
+def info(msg, options, verbose=False):
+    if (not verbose and not options.quiet) or (verbose and options.verbose):
+        print "INFO: %s" % msg
+
+def fail(msg):
+    print >> sys.stderr, "ERROR: %s" % msg
+    sys.exit(2)
 
 if __name__ == "__main__":
     main()
